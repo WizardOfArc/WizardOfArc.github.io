81ba3b2058ce63c796e09c33e5863ba202fbafa8
Azi <azicrawford@gmail.com>
2024-07-13 16:26:29 -0700

    generate table from json

--- js/index.js
+++ js/index.js
@@ -5,9 +5,36 @@ let woa_functions = {
         .then((response) => response.json())
         .then((json) => console.log(json));
     },
+    renderBandTable: (target) => {
+        function renderBandRow(rowData) {
+            let bandRow = document.createElement("div");
+            bandRow.className = "band-row";
+            let nameCell = document.createElement("div");
+            nameCell.className = "name-cell";
+            let instrumentList = document.createElement("div");
+            instrumentList.className = "instrument-list";
+            instrumentList.innerHTML = rowData.instruments.join(", ");
+            bandRow.appendChild(nameCell);
+            bandRow.appendChild(instrumentList);
+            return bandRow;
+        };
+        let bandTable = document.createElement("div");
+        let label = document.createTextNode("Members");
+        bandTable.appendChild(label);
+
+        fetch('./data/band_members.json')
+          .then((response) => response.json())
+          .then((data) => {
+                data.members.forEach( (member) => {
+                    bandTable.appendChild(renderBandRow(member));
+                });
+                target.appendChild(bandTable);
+          })
+    },
 };
 
 document.addEventListener("DOMContentLoaded", () => {
     woa_functions.dummyTest('I loaded the Doc');
-    woa_functions.loadJson('./data/band_members.json');
+    let bandMemberContainer = document.querySelector('#band-members');
+    woa_functions.renderBandTable(bandMemberContainer);
 })
