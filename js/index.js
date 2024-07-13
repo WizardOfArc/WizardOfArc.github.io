697967f4a98e3c1fc6aaede5b633e297f023ac81
Azi <azicrawford@gmail.com>
2024-07-13 16:07:11 -0700

    trying loading json on GH pages...

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+let woa_functions = {
+    dummyTest: (words) => console.log(`Dummy testing ${words}`),
+    loadJson: (filename) => {
+        fetch(filename)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    },
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('Doc loaded')
+    woa_functions.dummyTest('I loaded the Doc');
+    woa_functions.loadJson('./data/band_members.json');
 })
